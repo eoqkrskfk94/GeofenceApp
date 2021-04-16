32cf43abeb64cde123de44ba6dcec22377b45ca6
김명진 <mobinity.kmj@gmail.com>
2021-04-17 00:03:18 +0900

    [Add] SharedViewModel.kt

--- app/src/main/java/com/daniel/geofenceapp/DataStoreRepository.kt
+++ app/src/main/java/com/daniel/geofenceapp/DataStoreRepository.kt
@@ -9,15 +9,18 @@ import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.preferencesDataStore
 import com.daniel.geofenceapp.util.Constants.PREFERENCE_FIRST_LAUNCH
 import com.daniel.geofenceapp.util.Constants.PREFERENCE_NAME
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 import java.io.IOException
+import javax.inject.Inject
 
 private val Context.dataStore by preferencesDataStore(PREFERENCE_NAME)
 
-
-class DataStoreRepository(private val context: Context) {
+@ViewModelScoped
+class DataStoreRepository @Inject constructor(@ApplicationContext private val context: Context) {
 
     private object PreferenceKey {
         val firstLaunch = booleanPreferencesKey(PREFERENCE_FIRST_LAUNCH)
---
+++ app/src/main/java/com/daniel/geofenceapp/viewModels/SharedViewModel.kt
@@ -0,0 +1,30 @@
+package com.daniel.geofenceapp.viewModels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.daniel.geofenceapp.DataStoreRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SharedViewModel @Inject constructor(
+        application: Application,
+        private val dataStoreRepository: DataStoreRepository): AndroidViewModel(application) {
+
+    val app = application
+
+    //DataStore
+    val readFirstLaunch = dataStoreRepository.readFirstLaunch.asLiveData()
+
+    fun saveFirstLaunch(firstLaunch: Boolean) =
+            viewModelScope.launch(Dispatchers.IO) {
+                dataStoreRepository.saveFirstLaunch(firstLaunch)
+            }
+
+
+
+}
