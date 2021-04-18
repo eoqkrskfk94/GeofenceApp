656ef50ec185d0a1af66656fd16bee358a8a9253
김명진 <mobinity.kmj@gmail.com>
2021-04-18 21:50:22 +0900

    [Add] step 2 fragment 추가

--- app/src/main/java/com/daniel/geofenceapp/ui/addgeofence/Step2Fragment.kt
+++ app/src/main/java/com/daniel/geofenceapp/ui/addgeofence/Step2Fragment.kt
@@ -1,22 +1,100 @@
 package com.daniel.geofenceapp.ui.addgeofence
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.daniel.geofenceapp.R
+import com.daniel.geofenceapp.databinding.FragmentStep2Binding
+import com.daniel.geofenceapp.viewModels.SharedViewModel
+import com.google.android.gms.common.api.ApiException
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.AutocompleteSessionToken
+import com.google.android.libraries.places.api.model.TypeFilter
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest
+import com.google.android.libraries.places.api.net.PlacesClient
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 
 class Step2Fragment : Fragment() {
+
+    private var _binding: FragmentStep2Binding? = null
+    private val binding get() = _binding!!
+
+    private val sharedViewModel: SharedViewModel by activityViewModels()
+
+    private lateinit var placesClient: PlacesClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Places.initialize(requireContext(), getString(R.string.google_maps_key))
+        placesClient = Places.createClient(requireContext())
+    }
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_step2, container, false)
+    ): View {
+        _binding = FragmentStep2Binding.inflate(inflater, container, false)
+
+        binding.geofenceLocationEt.doOnTextChanged{text, _, _, _ ->
+            getPlaces(text)
+        }
+
+        binding.step2Back.setOnClickListener {
+            findNavController().navigate(R.id.action_step2Fragment_to_step1Fragment)
+        }
+
+        binding.step2Next.setOnClickListener {
+            findNavController().navigate(R.id.action_step2Fragment_to_step3Fragment)
+        }
+        return binding.root
+    }
+
+    private fun getPlaces(text: CharSequence?) {
+        if(sharedViewModel.checkDeviceLocationSettings(requireContext())){
+            lifecycleScope.launch {
+                if(text.isNullOrEmpty()){
+
+                }else{
+                    val token = AutocompleteSessionToken.newInstance()
+
+                    val request = FindAutocompletePredictionsRequest.builder()
+                            .setCountries(sharedViewModel.geoCountryCode)
+                            .setTypeFilter(TypeFilter.CITIES)
+                            .setSessionToken(token)
+                            .setQuery(text.toString())
+                            .build()
+                    placesClient.findAutocompletePredictions(request)
+                            .addOnSuccessListener { response ->
+
+                            }
+                            .addOnFailureListener { exception: Exception? ->
+                                if(exception is ApiException){
+                                    Log.e("Step2Fragment", exception.statusCode.toString())
+                                }
+                            }
+                }
+            }
+        }else{
+            Toast.makeText(requireContext(), "Please Enable Location Settings.", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
--- app/src/main/java/com/daniel/geofenceapp/viewModels/SharedViewModel.kt
+++ app/src/main/java/com/daniel/geofenceapp/viewModels/SharedViewModel.kt
@@ -1,6 +1,10 @@
 package com.daniel.geofenceapp.viewModels
 
 import android.app.Application
+import android.content.Context
+import android.location.LocationManager
+import android.os.Build
+import android.provider.Settings
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
@@ -28,6 +32,24 @@ class SharedViewModel @Inject constructor(
             viewModelScope.launch(Dispatchers.IO) {
                 dataStoreRepository.saveFirstLaunch(firstLaunch)
             }
+
+
+    fun checkDeviceLocationSettings(context: Context): Boolean {
+
+        return if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            locationManager.isLocationEnabled
+        }else{
+            val mode : Int = Settings.Secure.getInt(
+                    context.contentResolver,
+                    Settings.Secure.LOCATION_MODE,
+                    Settings.Secure.LOCATION_MODE_OFF
+            )
+
+            mode != Settings.Secure.LOCATION_MODE_OFF
+        }
+
+    }
 
 
 
